380b6a0d6ec1e841018fe2771d119e0f1adaf371
Dhruv Rajpoot <dhruv.r@hyperverge.co>
2024-12-12 11:54:45 +0530

    fix: accessibility, seo and best practices

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,60 +1,69 @@
 import { Route, Routes } from "react-router-dom";
-import SignInSignUpPage from "../pages/SignInSignUpPage";
-import SignUpPage from "../pages/SignUpPage";
-import HomePage from "../pages/HomePage";
-import BookingDetails from "../pages/BookingDetails";
-import BuyTicket from "../pages/BuyTicket/BuyTicket";
-import MovieDetailsPage from "../pages/MovieDetailsPage";
-import SignInPage from "../pages/SignInPage";
-import BuyFood from "../pages/BuyFood";
-import BookedTickets from "../pages/BookedTickets";
-import Favorites from "../pages/Favorites";
-import ProfilePage from "../pages/ProfilePage";
-import PrivateRoute from "./PrivateRoutes";
-import AvailableTheaters from "../pages/BuyTicket/AvailableTheaters";
-import AvailableSessions from "../pages/BuyTicket/AvailableSessions";
+import { Suspense, lazy } from "react";
+import Loading from "../pages/Loading";
+
+const SignInSignUpPage = lazy(() => import("../pages/SignInSignUpPage"));
+const SignUpPage = lazy(() => import("../pages/SignUpPage"));
+const HomePage = lazy(() => import("../pages/HomePage"));
+const BookingDetails = lazy(() => import("../pages/BookingDetails"));
+const BuyTicket = lazy(() => import("../pages/BuyTicket/BuyTicket"));
+const MovieDetailsPage = lazy(() => import("../pages/MovieDetailsPage"));
+const SignInPage = lazy(() => import("../pages/SignInPage"));
+const BuyFood = lazy(() => import("../pages/BuyFood"));
+const BookedTickets = lazy(() => import("../pages/BookedTickets"));
+const Favorites = lazy(() => import("../pages/Favorites"));
+const ProfilePage = lazy(() => import("../pages/ProfilePage"));
+const AvailableTheaters = lazy(
+  () => import("../pages/BuyTicket/AvailableTheaters")
+);
+const AvailableSessions = lazy(
+  () => import("../pages/BuyTicket/AvailableSessions")
+);
+const PrivateRoute = lazy(() => import("./PrivateRoutes"));
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/signinsignup" element={<SignInSignUpPage />} />
-      <Route path="/signin" element={<SignInPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/moviedetails/:id" element={<MovieDetailsPage />} />
-      <Route
-        path="/bookingdetails/:id"
-        element={<PrivateRoute element={<BookingDetails />} />}
-      />
-      <Route
-        path="/buyticket/:id"
-        element={<PrivateRoute element={<BuyTicket />} />}
-      />
-      <Route
-        path="/buyfood/:id"
-        element={<PrivateRoute element={<BuyFood />} />}
-      />
-      <Route
-        path="/bookedtickets"
-        element={<PrivateRoute element={<BookedTickets />} />}
-      />
-      <Route
-        path="/favorites"
-        element={<PrivateRoute element={<Favorites />} />}
-      />
-      <Route
-        path="/profile"
-        element={<PrivateRoute element={<ProfilePage />} />}
-      />
-      <Route
-        path="/available-theaters/:movieid"
-        element={<PrivateRoute element={<AvailableTheaters />} />}
-      />
-      <Route
-        path="/available-sessions/:movieid/:id"
-        element={<PrivateRoute element={<AvailableSessions />} />}
-      />
-    </Routes>
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signinsignup" element={<SignInSignUpPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/moviedetails/:id" element={<MovieDetailsPage />} />
+        <Route
+          path="/bookingdetails/:id"
+          element={<PrivateRoute element={<BookingDetails />} />}
+        />
+        <Route
+          path="/buyticket/:id"
+          element={<PrivateRoute element={<BuyTicket />} />}
+        />
+        <Route
+          path="/buyfood/:id"
+          element={<PrivateRoute element={<BuyFood />} />}
+        />
+        <Route
+          path="/bookedtickets"
+          element={<PrivateRoute element={<BookedTickets />} />}
+        />
+        <Route
+          path="/favorites"
+          element={<PrivateRoute element={<Favorites />} />}
+        />
+        <Route
+          path="/profile"
+          element={<PrivateRoute element={<ProfilePage />} />}
+        />
+        <Route
+          path="/available-theaters/:movieid"
+          element={<PrivateRoute element={<AvailableTheaters />} />}
+        />
+        <Route
+          path="/available-sessions/:movieid/:id"
+          element={<PrivateRoute element={<AvailableSessions />} />}
+        />
+      </Routes>
+    </Suspense>
   );
 };
 
